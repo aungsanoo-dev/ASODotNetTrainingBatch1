54a9826a62a3c10e0af1edf943f682b965d7246d
aungsanoo-dev <aungsanoo.aso.01@gmail.com>
2025-06-25 14:03:53 +0630

    Feat: Add project2

---
+++ ASODotNetTrainingBatch1.Project2.ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string jsonStr = File.ReadAllText("DreamDictionary.json");
+
+Console.WriteLine(jsonStr);
+Console.ReadLine();
